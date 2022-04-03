6869d25fd002807ce0c2132fd474e4c86f2f44a7
Jonathan Martens <jmartens8@yahoo.de>
2022-04-03 21:26:15 +0200

    safe

--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,40 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
 
   shuffleCards() {
-    // ... write your code here
-  }
+    let n = this.cards.length;
+    for (let i = n - 1; i > 0; i--){
+        // Pick a random index from 0 to i inclusive
+        let j = Math.floor(Math.random() * (i + 1));
 
+        // Swap arr[i] with the element
+        // at random index
+        [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+  }
+  
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked ++
+    if (card1 === card2){
+      this.pairsGuessed ++
+      return true
+    } else return false
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsClicked === 0){
+      return false
+    }
+    else if ((this.cards.length) / 2 === this.pairsGuessed){
+      return true
+    }
+    else return false
   }
 }
 
